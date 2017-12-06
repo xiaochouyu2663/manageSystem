2dbc3408d76ad5294a25380e0aef4e734e3a358a
orchief <18230373213@163.com>
2017-12-06 19:54:42 +0800

    这只是一次测试

--- src/router/routes.js
+++ src/router/routes.js
@@ -44,7 +44,7 @@ import chartsMix from '../views/Demo/charts/mix.vue'
 
 /**
  * meta参数解析
- * hideLeft: 是否隐藏左侧菜单，单页菜单为true
+ * hideLeft: 是 否隐藏左侧菜单，单页菜单为true
  * module: 菜单所属模块
  * menu: 所属菜单，用于判断三级菜单是否显示高亮，如菜单列表、添加菜单、编辑菜单都是'menu'，用户列表、添加用户、编辑用户都是'user'，如此类推
  */
